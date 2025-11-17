d905aa096701568681b0f9723816cdafe6ccd819
skxywtf <skxywtf@gmail.com>
2025-11-16 20:55:10 -0700

    Wrap app with ClerkProvider for Clerk sign-in

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import "./globals.css";
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "InfinityXZ",
