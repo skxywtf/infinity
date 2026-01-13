17e2067b2c02e69749b6d4721c43662b109d4717
RishikarthikVelliangiri <35276332+RishikarthikVelliangiri@users.noreply.github.com>
2026-01-12 19:44:23 -0600

    fix: Disable Middleware to debug Private DNS Error

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,13 @@
 
 import { withAuth } from "next-auth/middleware"
 
-export default withAuth({
-    callbacks: {
-        authorized: ({ token }) => !!token,
-    },
-    pages: {
-        signIn: '/login',
-    },
-})
+// export default withAuth({
+//     callbacks: {
+//         authorized: ({ token }) => !!token,
+//     },
+//     pages: {
+//         signIn: '/login',
+//     },
+// })
 
-export const config = { matcher: ["/experienz", "/app/infinity"] }
+// export const config = { matcher: ["/experienz", "/app/infinity"] }
