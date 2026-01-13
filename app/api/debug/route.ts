df6d98dd5fcc90c27332531503fd5dd70d9c03f0
RishikarthikVelliangiri <35276332+RishikarthikVelliangiri@users.noreply.github.com>
2026-01-12 19:31:00 -0600

    feat: Add Debug Endpoint

---
+++ app/api/debug/route.ts
@@ -0,0 +1,13 @@
+
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+    return NextResponse.json({
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        VERCEL_URL: process.env.VERCEL_URL,
+        NODE_ENV: process.env.NODE_ENV,
+        EMAIL_SERVER_DEFINED: !!process.env.EMAIL_SERVER,
+        GHOST_API_URL: process.env.GHOST_API_URL,
+        GHOST_ADMIN_KEY_DEFINED: !!process.env.GHOST_ADMIN_API_KEY
+    });
+}
